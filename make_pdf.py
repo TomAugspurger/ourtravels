e19f66f52c63118f37b99b9692ceea48040e9824
Tom Augspurger <tom.augspurger88@gmail.com>
2014-07-13 00:19:01 +0200

    add pdfjam call to make single pdf

--- make_pdf.py
+++ make_pdf.py
@@ -10,15 +10,22 @@ def main():
     pdfcontent = Path(__file__).parent / 'pdf/content/'
     pdfoutput = Path(__file__).parent / 'pdf/output/'
 
-    pdfcontent.mkdir(parents=True)
-    pdfoutput.mkdir(parents=True)
+    for f in [pdfcontent, pdfoutput]:
+        try:
+            f.mkdir(parents=True)
+        except FileExistsError:
+            pass
 
     for article in content.iterdir():
         shutil.copy(str(article), str(pdfcontent / article.name))
 
+    dates = []
+
     for article in pdfcontent.iterdir():
         with article.open('r') as f:
             header = ''.join(takewhile(lambda x: x != '\n', f))
+            d = [x for x in header.split('\n') if x[:4] == 'date'][0][-10:]
+            dates.append((article.stem, d))
             text = '---\n' + header + '---\n\n' + f.read()
 
         with article.open('w') as f:
@@ -26,7 +33,9 @@ def main():
 
         subprocess.call(["pandoc", str(article), "-o",
                          "pdf/output/{}.pdf".format(article.stem)])
-
+    dates = sorted(dates, key=lambda x: x[1])
+    subprocess.call(["pdfjam", "-o", "pdf/output/book.pdf"]
+                    + ["pdf/output/{}.pdf".format(x[0]) for x in dates])
 
 if __name__ == '__main__':
     main()
