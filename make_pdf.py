e794e5307e07eeb518309c0274766d8a5962a407
Tom Augspurger <tom.augspurger88@gmail.com>
2014-07-12 12:57:30 +0200

    put in main

--- make_pdf.py
+++ make_pdf.py
@@ -1,27 +1,32 @@
 # -*- coding: utf-8 -*-
-
-import os
 import shutil
-import pathlib
+from pathlib import Path
 import subprocess
 from itertools import takewhile
 
-content = Path(__file__).parent / 'content'
-pdfcontent = Path(__file__).parent / 'pdf/content/'
-pdfoutput = Path(__file__).parent / 'pdf/output/'
 
-pdfcontent.mkdir(parents=True)
-pdfoutput.mkdir(parents=True)
+def main():
+    content = Path(__file__).parent / 'content'
+    pdfcontent = Path(__file__).parent / 'pdf/content/'
+    pdfoutput = Path(__file__).parent / 'pdf/output/'
+
+    pdfcontent.mkdir(parents=True)
+    pdfoutput.mkdir(parents=True)
+
+    for article in content.iterdir():
+        shutil.copy(str(article), str(pdfcontent / article.name))
+
+    for article in pdfcontent.iterdir():
+        with article.open('r') as f:
+            header = ''.join(takewhile(lambda x: x != '\n', f))
+            text = '---\n' + header + '---\n\n' + f.read()
+
+        with article.open('w') as f:
+            f.write(text)
+
+        subprocess.call(["pandoc", str(article), "-o",
+                         "pdf/output/{}.pdf".format(article.stem)])
 
 
-for article in content.iterdir():
-    shutil.copy(str(article), str(pdfcontent / article.name))
-
-
-for article in pdfcontent.iterdir():
-    with article.open('r') as f:
-        header = ''.join(takewhile(lambda x: x != '\n', f))
-        text = '---\n' + header + '---\n\n' + f.read()
-
-    with article.open('w') as f:
-        f.write(text)
+if __name__ == '__main__':
+    main()
